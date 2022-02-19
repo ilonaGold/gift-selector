fc03f064c3741ffa922b4fbe97fa790cf0e3675a
Ilona Goldmane <songa17@inbox.lv>
2022-02-19 17:24:45 +0200

    Added form validation

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ let costEl = document.getElementById("cost-el");
 let foodSelect = document.getElementById("food-select");
 let transportSelect = document.getElementById("transport-select");
 let addBalloonCheckbox = document.getElementById("balloon-checkbox");
+let error = document.getElementById("error");
+let checkbox = document.getElementById("balloon-checkbox");
  
 document.getElementById("btn").addEventListener("click", calculate); 
 
 function calculate() {
+    if (foodSelect.value === "" || transportSelect.value === "") {
+        error.style.display = "block";
+        error.textContent = "💘 Please make your choice! 💘";
+    } else {
     let cost = parseInt(foodSelect.value) + parseInt(transportSelect.value) + addBalloon();
     costEl.textContent = `$${cost}`;
+    error.style.display = "none";
+    }
 }
  
 function addBalloon() {
